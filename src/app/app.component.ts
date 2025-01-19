910fdaccd2add33fa86db58e37c7281ff9f7dde3
Zuzky <sleepyzuzki@gmail.com>
2025-01-18 20:29:52 -0500

    Configure project with web awesome, pnpm and tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  title = 'ng-sleepy-zuzki';
+  title = 'Sleepy Zuzki';
 }
